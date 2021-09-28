83dc95d9e5cee4149478f31e49ce1fdb9d538f2d
Brad Trnavsky <bmtrnavsky@msn.com>
2021-09-28 12:05:43 -0500

    Create intro_to_Cpp.cpp

---
+++ intro_to_Cpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// this is a single line comment comment text is ignored by the computer
+//  std::cout << "prints between the quotes and gives you a new line\n";
+int main() 
+{
+  // you can use multiple std:cout to do multiple lines
+  // code runs line by line top to bottom. 
+  std::cout << "       1\n";
+  std::cout << "     2 3\n";
+  std::cout << "   4 5 6\n";
+  std::cout << "7 8 9 10\n";
+}
