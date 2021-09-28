6c998df68f716bd6d26b143a7384af455cf26eff
Brad Trnavsky <bmtrnavsky@msn.com>
2021-09-28 14:52:20 -0500

    Create cpp_notes_on_variables.cpp

---
+++ cpp_notes_on_variables.cpp
@@ -0,0 +1,22 @@
+/*
+A variable is simply a name that represents a particular piece of your computer’s memory that has been set aside for you to store, retrieve, and use data.
+
+  Type      Use                     Example
+
+  int       Interger Numbers        0, 399, 1000
+
+  double    floating-point numbers  3.14, -200.0
+
+  char      charicters              a, @, X, o
+
+  string    sequence of charicters  "Hello World!" "Brad"
+
+  bool      truth values            true false
+
+  
+*/
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
