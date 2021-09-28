a1d630c54ae3bc218ab7c98a95885acb2efc93eb
Brad Trnavsky <bmtrnavsky@msn.com>
2021-09-28 11:50:11 -0500

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
+#include <iostream> // Includes standard C input output libraries
 
-int main() {
-  std::cout << "Hello World!\n";
-}
+int main() { // The main function
+  std::cout << "Hello World!\n"; // what the program does in this case print "Hello World!"
+} // end of function
