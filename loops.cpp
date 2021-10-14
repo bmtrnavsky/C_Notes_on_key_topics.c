521979765c586bfb286f1c1a09ef890fe4874304
Brad Trnavsky <bmtrnavsky@msn.com>
2021-10-14 15:20:50 -0500

    Create loops.cpp

---
+++ loops.cpp
@@ -0,0 +1,14 @@
+/*The while loop looks very similar to an if statement. And just like an if statement, it executes the code inside of it if the condition is true.
+
+However, the difference is that the while loop will continue to execute the code inside of it, over and over again, as long as the condition is true.*/
+
+//example
+
+while (condition) {
+
+  statements
+
+}
+
+// Instead of executing something IF it is true it continues to execute so long as the condition continues to be true. 
+
